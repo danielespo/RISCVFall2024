1259d241d631d6e82a338899beb894fc8996f2af
Dan <dalbertico007@gmail.com>
2024-10-14 21:20:08 -0700

    continuing tomorrow, 16 instructions done

--- imem.sv
+++ imem.sv
@@ -1,18 +1,6 @@
 // Instruction Memory - 32bit width x 256 rows
 // Edit this to add more instructions
 
-// ALU comments : 
-// 0. ADD: 			y = a + b 		if alu_op=4'b0000 (funct7[5]=0, funct3[2:0]=000)
-// 1. Shift-Left-Logical:	y = a << b 		if alu_op=4'b0001 (funct7[5]=0, funct3[2:0]=001)
-// 2. Set-Less-Than (Signed):	y = (a < b)?1:0		if alu_op=4'b0010 (funct7[5]=0, funct3[2:0]=010)
-// 3. Set-Less-Than (Unsigned):	y = (a < b)?1:0 	if alu_op=4'b0011 (funct7[5]=0, funct3[2:0]=011)
-// 4. Bitwise XOR:		y = a ^ b		if alu_op=4'b0100 (funct7[5]=0, funct3[2:0]=100)
-// 5. Shift-Right-Logical:	y = a >> b(zero-extend)	if alu_op=4'b0101 (funct7[5]=0, funct3[2:0]=101)
-// 6. Shift-Right-Arithmetic:	y = a >> b(sign-extend)	if alu_op=4'b0110 (funct7[5]=1, funct3[2:0]=101)
-// 7. Bitwise OR:		y = a | b		if alu_op=4'b0111 (funct7[5]=1, funct3[2:0]=110)
-// 8. Bitwise AND:		y = a & b		if alu_op=4'b1000 (funct7[5]=0, funct3[2:0]=111)
-// 9. SUB:			y = a - b		if alu_op=4'b1001 (funct7[5]=1, funct3[2:0]=000)
-
 `timescale 1ns/1ps
 
 module imem (pc, inst);
@@ -25,22 +13,15 @@ integer i;
 
 // Initializing with a list of instructions to execute
 // Sajeeb, I am getting instructions from here
-// NOTE: Change this and add the instructions that are missing\
+// NOTE: Change this and add the instructions that are missing
 
 // https://msyksphinz-self.github.io/riscv-isadoc/html/rvi.html#xor
-
-// RV32I Base Integer instructions
-
-// Basically:
-// funct 7, rs 5, rs1 5, funct 3, rd 5, opcode 7
-
-// funct7 : 7 bits indicate instruction
 // https://www.cs.sfu.ca/~ashriram/Courses/CS295/assets/notebooks/RISCV/RISCV_CARD.pdf
 
 // Now, showcasing 20 different instructions
 initial
 begin
-	// D Type Instructions (10)
+	// R Type Instructions (10)
 	
 	// add x3 x1 x2:	[funct7=0000000][rs2(x2) ][rs1(x1)][funct3=000][rd(x3)][opcode=0110011]
 	mem[0] <= 		32'b0000000_00010_00001_000_00011_0110011;
@@ -53,31 +34,48 @@ begin
 	// and x3 x1 x2:	[funct7=0000000][rs2(x2)][rs1(x1)][funct3=111][rd(x3)][opcode=0110011]
 	mem[4] <=		32'b0000000_00010_00001_111_00011_0110011;
 	// sll x3 x1 x2:	[funct7=0000000][rs2(x2)][rs1(x1)][funct3=001][rd(x3)][opcode=0110011]
-	mem[4] <=		32'b0000000_00010_00001_001_00011_0110011;
+	mem[5] <=		32'b0000000_00010_00001_001_00011_0110011;
 	// srl x3 x1 x2:	[funct7=0000000][rs2(x2)][rs1(x1)][funct3=101][rd(x3)][opcode=0110011]
-	mem[4] <=		32'b0000000_00010_00001_101_00011_0110011;
+	mem[6] <=		32'b0000000_00010_00001_101_00011_0110011;
 	// sra x3 x1 x2:	[funct7=0000010][rs2(x2)][rs1(x1)][funct3=101][rd(x3)][opcode=0110011] (msb-extends)
-	mem[4] <=		32'b0000010_00010_00001_101_00011_0110011;
+	mem[7] <=		32'b0000010_00010_00001_101_00011_0110011;
 	// slt x3 x1 x2:	[funct7=0000000][rs2(x2)][rs1(x1)][funct3=010][rd(x3)][opcode=0110011]
-	mem[4] <=		32'b0000000_00010_00001_010_00011_0110011;
+	mem[8] <=		32'b0000000_00010_00001_010_00011_0110011;
 	// sltu x3 x1 x2:	[funct7=0000000][rs2(x2)][rs1(x1)][funct3=011][rd(x3)][opcode=0110011]
-	mem[4] <=		32'b0000000_00010_00001_011_00011_0110011;
+	mem[9] <=		32'b0000000_00010_00001_011_00011_0110011;
 
-	// RV32M Multiply Extension
-	// mul x3 x1 x2:	[funct7=0000010][rs2(x2)][rs1(x1)][funct3=000][rd(x3)][opcode=0110011]
+	// RV32M Multiply Extension (3)
+	// mul x3 x1 x2:	[funct7=0000001][rs2(x2)][rs1(x1)][funct3=000][rd(x3)][opcode=0110011]
+	mem[10] <=		32'b0000001_00010_00001_000_00011_0110011;
+	// div x3 x1 x2:	[funct7=0000001][rs2(x2)][rs1(x1)][funct3=100][rd(x3)][opcode=0110011]
+	mem[11] <=		32'b0000001_00010_00001_100_00011_0110011;
+	// rem x3 x1 x2:	[funct7=0000001][rs2(x2)][rs1(x1)][funct3=110][rd(x3)][opcode=0110011]
+	mem[12] <=		32'b0000001_00010_00001_110_00011_0110011;
+
+
 
 
 	// I Type Instructions
-	// lw x7 0(x6):		[imm(11:5)     ][imm(4:0)][rs1(x6)][funct3=010][rd(x7)  ][opcode=0000011]
-	mem[3] <=		32'b0000000_00000_00110_010_00111_0000011;
 	
+	// addi x7 0(x6):		[imm(11:5)][imm(4:0)][rs1(x6)][funct3=010][rd(x7)][opcode=0000011]
+	mem[13] <=		32'b0000001_00010_00001_000_00011_0110011;
+	// xori
+	mem[13] <=		32'b0000001_00010_00001_000_00011_0110011;
+
+
+	// Is this imm(11:5)? Do not think so... rd = M[rs1+imm][0:31] right? Might be missing something
+	// lw x7 0(x6):		[imm(11:5)][imm(4:0)][rs1(x6)][funct3=010][rd(x7)][opcode=0000011]
+	mem[14] <=		32'b0000000_00000_00110_010_00111_0000011;
+
+
+
 	// S Type Instructions
 	// sw x5 0(x6):		[imm(11:5)     ][rs2(x5) ][rs1(x6)][funct3=010][imm(4:0)][opcode=0100011]
-	mem[4] <=		32'b0000000_00101_00110_010_00000_0100011;
+	mem[15] <=		32'b0000000_00101_00110_010_00000_0100011;
 	
 	// B Type Instructions
-	
-	mem[5] <=
+	// beq Branch == B 1100011 0x0 if(rs1 == rs2) PC += imm
+	// beq x5 0(x6) [imm(11:5)     ][rs2(x5) ][rs1(x6)][funct3=010][imm(4:0)][opcode=0100011]
 
 
 	for (i=4; i<256; i=i+1)
