f845253c83be04eddc5b6c7f7181af889e0ec392
Dan <dalbertico007@gmail.com>
2024-10-20 22:38:44 -0700

    finished instructions, we are doing full word, 13

--- imem.sv
+++ imem.sv
@@ -1,6 +1,6 @@
 // Instruction Memory - 32bit width x 256 rows
-// Edit this to add more instructions
-
+// Daniel Espinosa, Mahmudul Sajeeb, Prof. Bongjin Kim 2024
+// MIT License
 `timescale 1ns/1ps
 
 module imem (pc, inst);
@@ -8,21 +8,16 @@ module imem (pc, inst);
 input [31:0] pc;
 output [31:0] inst;
 
-reg [31:0] mem[255:0];
+reg [31:0] mem[31:0];
 integer i;
 
-// Initializing with a list of instructions to execute
-// Sajeeb, I am getting instructions from here
-// NOTE: Change this and add the instructions that are missing
-
+// Useful RISC-V Sources:
 // https://msyksphinz-self.github.io/riscv-isadoc/html/rvi.html#xor
 // https://www.cs.sfu.ca/~ashriram/Courses/CS295/assets/notebooks/RISCV/RISCV_CARD.pdf
 
-// Now, showcasing 20 different instructions
 initial
 begin
 	// R Type Instructions (10)
-	
 	// add x3 x1 x2:	[funct7=0000000][rs2(x2) ][rs1(x1)][funct3=000][rd(x3)][opcode=0110011]
 	mem[0] <= 		32'b0000000_00010_00001_000_00011_0110011;
 	// sub x5 x3 x4:	[funct7=0100000][rs2(x4) ][rs1(x3)][funct3=000][rd(x5)][opcode=0110011]
@@ -44,46 +39,24 @@ begin
 	// sltu x3 x1 x2:	[funct7=0000000][rs2(x2)][rs1(x1)][funct3=011][rd(x3)][opcode=0110011]
 	mem[9] <=		32'b0000000_00010_00001_011_00011_0110011;
 
-	// RV32M Multiply Extension (3)
-	// mul x3 x1 x2:	[funct7=0000001][rs2(x2)][rs1(x1)][funct3=000][rd(x3)][opcode=0110011]
-	mem[10] <=		32'b0000001_00010_00001_000_00011_0110011;
-	// div x3 x1 x2:	[funct7=0000001][rs2(x2)][rs1(x1)][funct3=100][rd(x3)][opcode=0110011]
-	mem[11] <=		32'b0000001_00010_00001_100_00011_0110011;
-	// rem x3 x1 x2:	[funct7=0000001][rs2(x2)][rs1(x1)][funct3=110][rd(x3)][opcode=0110011]
-	mem[12] <=		32'b0000001_00010_00001_110_00011_0110011;
-
-
-
-
-	// I Type Instructions
-	
-	// addi x7 0(x6):		[imm(11:5)][imm(4:0)][rs1(x6)][funct3=010][rd(x7)][opcode=0000011]
-	mem[13] <=		32'b0000001_00010_00001_000_00011_0110011;
-	// xori
-	mem[13] <=		32'b0000001_00010_00001_000_00011_0110011;
-
-
-	// Is this imm(11:5)? Do not think so... rd = M[rs1+imm][0:31] right? Might be missing something
+	// I Type Instructions (1)
 	// lw x7 0(x6):		[imm(11:5)][imm(4:0)][rs1(x6)][funct3=010][rd(x7)][opcode=0000011]
-	mem[14] <=		32'b0000000_00000_00110_010_00111_0000011;
+	mem[10] <=		32'b0000000_00000_00110_010_00111_0000011;
 
-
-
-	// S Type Instructions
+	// S Type Instructions (1)
 	// sw x5 0(x6):		[imm(11:5)     ][rs2(x5) ][rs1(x6)][funct3=010][imm(4:0)][opcode=0100011]
-	mem[15] <=		32'b0000000_00101_00110_010_00000_0100011;
+	mem[11] <=		32'b0000000_00101_00110_010_00000_0100011;
 	
-	// B Type Instructions
-	// beq Branch == B 1100011 0x0 if(rs1 == rs2) PC += imm
-	// beq x5 0(x6) [imm(11:5)     ][rs2(x5) ][rs1(x6)][funct3=010][imm(4:0)][opcode=0100011]
+	// B Type Instructions (1)
+	// beq x8 0(x6):	[imm[12|10:5]     ][rs2(x8) ][rs1(x6)][funct3=000][imm(4:1|11)][opcode=1100011]
+	mem[12] <=		32'b0000000_01000_00110_000_00000_1100011;
 
-
-	for (i=4; i<256; i=i+1)
+	for (i=13; i<32; i=i+1) // changed because 32 rows now not 256
 		mem[i] = 32'h0000_0000;
 
 	// 
 end
 
-assign inst = mem[pc[31:2]];	// Upper 30bits of PC = Row Address of IMEM - Lower 2bits: Column Byte Address
+assign inst = mem[pc[31:2]];	// Upper 30bits of PC = Row Address of IMEM - Lower 2bits: Column Byte Address (for half word multiplexing!)
 
 endmodule
